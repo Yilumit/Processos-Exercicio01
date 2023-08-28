f2ca3f138cc4abf477d2de9eae031160089af3a4
Vini <viniciussilvabarbosa2003@gmail.com>
2023-08-27 23:23:50 -0300

    Primeiro Commit
    Criação do repositório

---
+++ src/controller/RedesController.java
@@ -0,0 +1,55 @@
+package controller;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+public class RedesController {
+
+	public RedesController() {
+		super();
+	}
+
+	private String os() {
+		String os = System.getProperty("os.name");
+		return os;
+	}
+	
+	public void ip() {
+		String adaptador = "";
+		String os = os();
+		if (os.contains("Windows")) {
+			try {
+				Process ipconfig = Runtime.getRuntime().exec("ipconfig");//Recebe o fluxo de dados do processo 
+				InputStream fluxo = ipconfig.getInputStream(); //Recebe o fluxo de dados codificados em bits
+				InputStreamReader leitor = new InputStreamReader(fluxo); //Lê o fluxo de saída de console do processo e converte para String
+				BufferedReader buffer = new BufferedReader(leitor);
+				String linha = buffer.readLine();				
+				while (linha != null) {
+					
+					if (linha.contains("Adaptador")) {
+//						sBuffer.append(linha);
+						adaptador = linha;
+					}
+					if (linha.contains("IPv4")) {
+//						sBuffer.append(linha);
+						adaptador = adaptador + linha;
+						System.out.println(adaptador);
+						adaptador = "";
+					}
+					linha = buffer.readLine();
+				}
+				buffer.close();
+				leitor.close();
+				fluxo.close();
+				
+				
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			
+		}
+	}
+	
+}
---
+++ src/view/Main.java
@@ -0,0 +1,15 @@
+package view;
+
+import controller.RedesController;
+
+public class Main {
+
+	public static void main(String[] args) {
+		RedesController redesController = new RedesController();
+		
+		redesController.ip();
+		
+
+	}
+
+}
