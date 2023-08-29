34ec9842c463e194882cda3fefaa63fdc795ab7d
Vini <viniciussilvabarbosa2003@gmail.com>
2023-08-29 01:08:39 -0300

    Update RedesController.java
    Finalização do método ip

--- src/controller/RedesController.java
+++ src/controller/RedesController.java
@@ -17,10 +17,11 @@ public class RedesController {
 	}
 	
 	public void ip() {
-		String adaptador = "";
 		String os = os();
 		if (os.contains("Windows")) {
 			try {
+				String adaptador = "";
+				
 				Process ipconfig = Runtime.getRuntime().exec("ipconfig");//Recebe o fluxo de dados do processo 
 				InputStream fluxo = ipconfig.getInputStream(); //Recebe o fluxo de dados codificados em bits
 				InputStreamReader leitor = new InputStreamReader(fluxo); //Lê o fluxo de saída de console do processo e converte para String
@@ -46,7 +47,38 @@ public class RedesController {
 				e.printStackTrace();
 			}
 			
+		}else {
+			try {
+				
+				Process ipaddr = Runtime.getRuntime().exec("ip addr");//Recebe o fluxo de dados do processo 
+				InputStream fluxo = ipaddr.getInputStream(); //Recebe o fluxo de dados codificados em bits
+				InputStreamReader leitor = new InputStreamReader(fluxo); //Lê o fluxo de saída de console do processo e converte para String
+				BufferedReader buffer = new BufferedReader(leitor);
+				String linha = buffer.readLine();				
+				while (linha != null) {
+					String[] adaptador = {};
+					if (linha.contains("mtu")) {
+						adaptador = linha.split(": ");
+					}
+					if (linha.contains("inet ")) {
+						String[] ipv4 = linha.split(" ");
+						System.out.println(adaptador[1]+"\nIPv4: "+ipv4[1]);
+					}
+					linha = buffer.readLine();
+				}
+				buffer.close();
+				leitor.close();
+				fluxo.close();
+				
+				
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
 		}
+	}
+	
+	public void ping() {
+		
 	}
 	
 }
